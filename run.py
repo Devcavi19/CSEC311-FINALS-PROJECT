afd0c557b3df99907b8fd697b00cbd51aad8ba9e
Herald Carl Avila <144099137+Devcavi19@users.noreply.github.com>
2025-09-25 21:37:32 +0000

    Initialize database on first run and update app run configuration for production

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
-from btbs import app
+from btbs import app, db
+import os
+
+# Initialize database on first run
+if os.getenv('FLASK_ENV') == 'production':
+    with app.app_context():
+        db.create_all()
+
+# For Vercel deployment
+application = app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False if os.getenv('FLASK_ENV') == 'production' else True)
