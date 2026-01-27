5f58c2b5bdc7b0cce86469965d0c0d3059ea2003
Herald Carl Avila <heavila@my.cspc.edu.ph>
2026-01-27 15:15:48 +0800

    Fixed the problems in this files

--- src/pages/admin/buses/AddBus.tsx
+++ src/pages/admin/buses/AddBus.tsx
@@ -1,4 +1,4 @@
-import { useState, FormEvent } from 'react';
+import { useState, type FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { busesAPI } from '../../../api';
 import FlashMessage from '../../../components/FlashMessage';
